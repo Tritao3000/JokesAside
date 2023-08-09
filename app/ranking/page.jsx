2eae817efd499b9373c4f98005eca0ef5e1f2cb0
Tiago Pereira <tiagorp2004@gmail.com>
2023-08-09 02:09:43 +0100

    Rank updates

--- app/ranking/page.jsx
+++ app/ranking/page.jsx
@@ -1,17 +1,22 @@
 "use client";
-
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Ranking = () => {
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
+  const { data: session } = useSession();
 
-  const handleProfileClick = () => {
-    // ! Código Repetido
-    if (post.creator._id === session?.user.id) return router.push("/profile");
-
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  const handleProfileClick = (post) => {
+    if (post.creator._id === session?.user.id) {
+      router.push("/profile");
+    } else {
+      router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    }
   };
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
@@ -28,27 +33,36 @@ const Ranking = () => {
   );
 
   return (
-    <div className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
-      {rankedPosts.map((post, index) => {
-        return (
-          <div
-            key={index}
-            className="flex flex-row gap-3 cursor-pointer"
-            onClick={handleProfileClick}
-          >
+    <div className="mt-10 w-full max-w-2xl flex flex-col gap-7 border border-slate-300  p-6 rounded-xl bg-slate-300/10">
+      {rankedPosts.map((post, index) => (
+        <div
+          key={index}
+          className="flex justify-between items-center p-3 rounded-lg bg-transparent hover:border border-slate-300 transition duration-300"
+        >
+          <div key={index} className="flex items-center gap-3">
             <Image
               src={post.creator.image}
               alt="user_profile_image"
               width={25}
               height={25}
-              className="rounded-full object-contain"
+              className="rounded-full object-contain cursor-pointer"
+              onClick={() => handleProfileClick(post)} // Pass the post to handleProfileClick
             />
             <h1 className="font-satoshi font-semibold text-slate-300">
               {post.creator.username}
             </h1>
           </div>
-        );
-      })}
+          <div className="flex items-center gap-3">
+            <p className="font-inter inline-block text-sm text-slate-300">
+              {post.userLiked.length}{" "}
+              {post.userLiked.length === 1 ? "Laugh" : "Laughs"}
+            </p>
+            <p className="font-inter font-bold text-sm title_gradient hover:text-slate-300 cursor-pointer">
+              See Post
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
